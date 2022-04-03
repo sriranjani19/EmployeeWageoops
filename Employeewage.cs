fb22f7c0cf1ffc0974f78d6542a0121d7f88e2d8
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-03 22:09:19 +0530

    Add files via upload

---
+++ Employeewage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class program
+    {
+        public static voidMain(string[] args)
+        {
+            Console.WriteLine("Welecome to Employee Wage Computation Using OOPS");
+
+            EmployeeAttendance empAttendance = new EmployeeAttendance();
+            empAttendance.PresentorAbsent();
+
+            //EmployeeWage empWage = new EmployeeWage();
+            //empWage.calEmployee();
+
+            //EmpSwitchcase empSwitchcase =new EmpSwitchcase();
+            //empSwitchcase.CalEmployeeSwitch();
+
+            //EmpWagePerMonth empWagePerMonth =new EmpWagePerMonth();
+            //empWagePerMonyh.CalEmployeeWagePerMonth();
+
+            EmpMonthlyWageCalc empMonthlyWageCalc = new EmpMonthlyWageCalc();
+            empMonthlyWageCalc.EmpWageCalcWithHourOrDays();
+        }
+    }
+}
+
+        
+           
+      
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book \n");
+
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.AddressBook();
+
+            //AddUsingConsole addUsingConsole = new AddUsingConsole();
+            //addUsingConsole.readUserDetails();
+
+            EditPersonDetails editPersonDetails = new EditPersonDetails();
+            editPersonDetails.editUserDetails();
+
+        }
+    }
+}
+
