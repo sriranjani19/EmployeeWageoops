64e7a112527869b7c758cf0dda2284d504d67137
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-12 09:56:12 +0530

    sri ranjani

---
+++ EmpWagePerMonth.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmpWagePerMonth
+    {
+        public void CalEmployeeWagePerMonth()
+        {
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int EMP_DAYS_PER_MONTH = 20;
+
+            int empHrs = 0;
+            int empWagePerMonth = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWagePerMonth = (empHrs * EMP_RATE_PER_HOUR) * EMP_DAYS_PER_MONTH;
+            Console.WriteLine("Emp Wage Per Month : " + empWagePerMonth);
+        }
+    }
+
+}
