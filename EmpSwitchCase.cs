9d69cda5442ebe75cf43ed6450f1b4bc0f14bcbc
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-12 10:06:23 +0530

    Add files via upload

---
+++ EmpSwitchCase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmpSwitchCase
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+
+        public void CalEmployeeSwitch()
+        {
+
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                case EMP_RATE_PER_HOUR:
+                    empHrs = 0;
+                    break;
+
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage Using Switch Case : " + empWage);
+        }
+    }
+}
+
